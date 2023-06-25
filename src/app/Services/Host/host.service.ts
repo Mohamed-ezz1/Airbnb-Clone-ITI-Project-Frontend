ae62baa8500c309a81e166c7efe00a6c7107025f
Mohamed Ezz Elarab <moh.ezzelarab@gmail.com>
2023-06-25 21:49:45 +0300

    add host service

--- src/app/Services/Host/host.service.ts
+++ src/app/Services/Host/host.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HostService {
 
-  constructor() { }
+  constructor(private myClient: HttpClient) { }
+
+  private readonly baseUrl = "https://localhost:7108/HostBooking"
+
+  //HostBookingsDto
+  // GetBookingByUserId(): Observable<HostBookingsDto[]> {
+  //   return this.myClient.get(this.baseUrl);
+  // }
 }
