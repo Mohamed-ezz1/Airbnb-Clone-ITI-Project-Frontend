0b7874405678a41c8589165c0ec05e6d5886f178
Mohamed Ezz Elarab <moh.ezzelarab@gmail.com>
2023-06-25 11:05:29 +0300

    Building project folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponentComponent } from './Components/Home/home-component/home-component.component';
-import { GttComponent } from './Components/Home/gtt/gtt.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponentComponent,
-    GttComponent
   ],
   imports: [
     BrowserModule,
