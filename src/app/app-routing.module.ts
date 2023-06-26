471fad60133fd1477db7c93527ef1e7158f2ad01
Mohamed Hossam <132598686+mhHossam@users.noreply.github.com>
2023-06-26 10:59:59 +0300

    add route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './Components/Login-Register/Register/register/register.component';
 import { LoginComponent } from './Components/Login-Register/Login/login/login.component';
+import { HostBookingOrdersComponent } from './Components/User/host-booking-orders/host-booking-orders.component';
+import { HostPropertiesComponent } from './Components/User/host-properties/host-properties.component';
 
 const routes: Routes = [
   {path:'Register', component:RegisterComponent},
-  {path :'login', component:LoginComponent}
+  {path :'login', component:LoginComponent},
+  {path:'hostBooking',component:HostBookingOrdersComponent},
+  {path:'hostProperty' , component:HostPropertiesComponent}
 
 ];
 
