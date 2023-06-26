707f09f4209e10463a42e6cbeeb7cac9993e735f
Mohamed Ezz Elarab <moh.ezzelarab@gmail.com>
2023-06-26 11:12:39 +0300

    edit host bookings

--- src/app/Components/User/host-booking-orders/host-booking-orders.component.ts
+++ src/app/Components/User/host-booking-orders/host-booking-orders.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, OnInit } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { HostService } from 'src/app/Services/Host/host.service';
 
 @Component({
@@ -7,15 +12,32 @@ import { HostService } from 'src/app/Services/Host/host.service';
   styleUrls: ['./host-booking-orders.component.css']
 })
 export class HostBookingOrdersComponent implements OnInit {
-  hostBooking: any;
+  displayedColumns: string[] = ['name', 'email', 'phone', 'address', 'edit', 'delete'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
 
   constructor(private hostDataService: HostService) {
   }
   ngOnInit(): void {
     this.hostDataService.GetBookingByUserId().subscribe({
-      next: (data) => { this.hostBooking = data },
+      next: (data: any) => {
+        this.dataSource = new MatTableDataSource<any>(data),
+          this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      },
       error: (error) => { console.log(error) },
-      complete: () => { console.log("complete") }
+      complete: () => { console.log("Get Host Property Completed") }
     })
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
