22f810d9506fabb03dc04f56ead58467b111b883
Eslam Badr <132064097+EslaamBadr@users.noreply.github.com>
2023-07-02 21:12:29 +0300

    prop details

--- src/app/Components/PropertyDetails/prop-details/prop-details.component.ts
+++ src/app/Components/PropertyDetails/prop-details/prop-details.component.ts
@@ -1,60 +1,71 @@
-import { Component, OnInit, ViewChild  } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PropertyService } from 'src/app/Services/Property/property.service';
 import { MaterialModule } from 'src/app/AngularMaterial/material.module'
-import { MatCalendar } from '@angular/material/datepicker';
-import { FormGroup, FormControl, FormBuilder  } from '@angular/forms';
+import { MatDatepickerInputEvent  } from '@angular/material/datepicker';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-prop-details',
   templateUrl: './prop-details.component.html',
   styleUrls: ['./prop-details.component.css'],
-  
+
 })
 export class PropDetailsComponent implements OnInit {
 
-  propDetails: any;
-  ID:any;
-  totalPrice: any;
-  numOfNights: any;
+  propDetails: any;             //all details of property
+  ID: any;                      //Id of property
+  totalPrice: any;              //total price of booking 
+  numOfNights: any;             //number of nights the user reserving 
+  numOfGuests: any = 2;         // number of guests the user reserving
+  isPDisabled!: boolean;        
+  isMDisabled!: boolean;
+  minDate = new Date();         //now date
+  startDate: any;               //start date of reservation
+  endDate: any;                 //end date of reservation
   range!: FormGroup;
-  // range: FormGroup = new FormGroup
-  // ({
-  //   start: new FormControl(),
-  //   end: new FormControl()
-  // });
-  constructor(myRoute:ActivatedRoute,private propService: PropertyService, private fb: FormBuilder) {
-    this.ID=myRoute.snapshot.params["id"];
+  
+  constructor(myRoute: ActivatedRoute, private propService: PropertyService,
+     private fb: FormBuilder) {
+
+    this.ID = myRoute.snapshot.params["id"];
     this.range = this.fb.group({
       start: null,
       end: null
     });
   }
 
-
-  dateFilter = (date: Date): boolean => {
-    const startDate = new Date(2023, 7, 1); // July 1, 2023
-    const endDate = new Date(2023, 7, 15); // July 15, 2023
-    const dd = Date.now();
-    return date >= startDate && date <= endDate;
-  }
-
-
   ngOnInit(): void {
     this.propService.GetPropertyById(this.ID).subscribe({
       next: (data) => { this.propDetails = data; console.log(data); },
       error: (error) => { console.log(error) },
-      complete: () => { console.log("complete");}
+      complete: () => { console.log("complete"); }
     })
   }
 
-    
-  // hh() {
-  //   const startDate = this.range.get('start')?.value;
-  //   const endDate = this.range.get('end')?.value;
-  //   console.log(startDate);
-  //   console.log(endDate);
-  //   // Do something with the selected date range
+  // plus button function
+  pButton() {
+    if (this.numOfGuests < this.propDetails.maxNumOfGuest) {
+      this.numOfGuests += 1;
+      this.isMDisabled = false;
+      console.log(this.numOfGuests)
+      if (this.numOfGuests == this.propDetails.maxNumOfGuest) {
+        this.isPDisabled = true;
+      }
+    }
+  }
+
+  // minus button function
+  mButton() {
+    if (this.numOfGuests > 1) {
+      this.numOfGuests -= 1;
+      this.isPDisabled = false;
+      console.log(this.numOfGuests)
+      if (this.numOfGuests == 1) {
+        this.isMDisabled = true;
+      }
+    }
+  }
 
 }
 
