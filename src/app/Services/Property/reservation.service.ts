f01933a1f8df37828e056deb656836ddb466c502
Eslam Badr <132064097+EslaamBadr@users.noreply.github.com>
2023-07-02 22:47:06 +0300

    reserve service

--- src/app/Services/Property/reservation.service.ts
+++ src/app/Services/Property/reservation.service.ts
@@ -4,13 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ReservationService {
-
-  totalPrice: any;              //total price of booking 
-  numOfNights: any;             //number of nights the user reserving 
-  numOfGuests: any = 2;         // number of guests the user reserving
-  startDate: any;               //start date of reservation
-  endDate: any;
-  userId: any;
-  
   constructor() { }
+
 }
