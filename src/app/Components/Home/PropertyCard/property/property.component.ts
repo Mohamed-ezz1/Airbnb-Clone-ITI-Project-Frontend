bc6917ca3195d6fc2441b5342f9c73be14d486c3
amrmekawii <65632088+amrmekawii@users.noreply.github.com>
2023-07-06 10:15:01 +0300

    kk

--- src/app/Components/Home/PropertyCard/property/property.component.ts
+++ src/app/Components/Home/PropertyCard/property/property.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component ,Input} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { async } from 'rxjs';
+import { CategoryService } from 'src/app/Services/Category/category.service';
 import { HostService } from 'src/app/Services/Host/host.service';
 import { PropertyService } from 'src/app/Services/Property/property.service';
 import { AuthenticationService } from 'src/app/Services/User/user.service';
@@ -16,13 +17,17 @@ import { QueryService } from 'src/app/Services/query/query.service';
 export class PropertyComponent {
   Property: any;
   ID: any;
+ objectcat :any = {"catogreyId": null}
+category2! :string;
+
+
   ///cahnge\\\
   PropertyFilter: any;
   objectFilter: any;
   constructor(private myRoute: ActivatedRoute,
     private query: QueryService, private PropertyService: PropertyService,
     private router: Router, private toastr: ToastrService,
-    private usertype: UsertypeService, private hostservice: HostService) {
+    private usertype: UsertypeService, private hostservice: HostService ,private stayService :CategoryService) {
     this.ID = myRoute.snapshot.params['id'];
     console.log(this.ID)
     this.usertype.getusertype().subscribe((user: any) => {
@@ -51,6 +56,7 @@ if (this.router.url=="/Property/filter"){
   console.log("lalalalalalal")
 }
 
+         if(this.ID == null ){
 
     this.PropertyService.GetAllPorperty().subscribe({
       next: (data) => {
@@ -60,7 +66,11 @@ if (this.router.url=="/Property/filter"){
 
 
     });
+  
+  }
     ///////////
+
+            if(this.ID == 1 ){
 
     if (this.query.query.value !== null) {
       console.log(this.query.query)
@@ -71,18 +81,37 @@ if (this.router.url=="/Property/filter"){
           console.log(this.query.query)
           this.objectFilter = this.query.query;
           console.log("object seacrcgggg");
-          this.search();
+          this.search(this.objectFilter);
         },
         error: () => console.log("Asdasdsadsa")
       });
 
     }
   }
+  if(this.ID ==2 ){
+
+this.stayService.query$.subscribe(
+  {
+    next: (value) => {
+
+      this.objectcat["catogreyId"] = this.stayService.queryforcategory;
+      console.log("object gategory");
+      console.log(this.objectcat);
+
+      this.search(this.objectcat);
+    },
+    error: () => console.log("Asdasdsadsa")
+  });
+  
+
+  }
+
+}
 
 
 
-  search() {
-    this.PropertyService.GetPropertySarch(this.objectFilter).subscribe({
+  search(objectFilter: any) {
+    this.PropertyService.GetPropertySarch(objectFilter).subscribe({
       next: (data) => {
         console.log("data filter")
 
@@ -96,13 +125,7 @@ if (this.router.url=="/Property/filter"){
       error: (e) => {
 
         console.log(this.objectFilter)
-
-        this.router.navigateByUrl('Property')
-
         this.toastr.warning("No Data Match")
-
-
-
         console.log(e)
       }
 
